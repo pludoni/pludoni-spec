60e8cf50db3be972c3e81f674f563b0ffea6aa97
Stefan Wienert <stwienert@gmail.com>
2014-01-12 13:32:58 +0100

    database cleaner

--- lib/pludoni/database-cleaner.rb
+++ lib/pludoni/database-cleaner.rb
@@ -5,14 +5,11 @@ RSpec.configure do |config|
   config.before(:suite) do
     DatabaseCleaner.clean_with(:truncation)
   end
-  config.before(:each) do
-    DatabaseCleaner.strategy = :transaction
-  end
-  config.before(:each) do
-    DatabaseCleaner.start
-  end
 
-  config.after(:each) do
+  config.around(:each) do |example|
+    DatabaseCleaner.strategy = example.metadata[:js] ? :truncation : :transaction
+    DatabaseCleaner.start
+    example.run
     DatabaseCleaner.clean
   end
 end
